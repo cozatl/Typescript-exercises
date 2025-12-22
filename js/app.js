5451d49443c5f8fa70c17fddbb6e00d72f72f3df
jesus cozatl <cozatlram@yahoo.com.mx>
2025-12-22 16:06:19 -0600

    Added changes to improve the code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function formatToDDMMYYYY(date) {
-    var day = String(date.getDate()).padStart(2, '0');
-    var month = String(date.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed
-    var year = date.getFullYear();
-    return "".concat(day, "/").concat(month, "/").concat(year); // Or `${day} ${month} ${year}` for space separation
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`; // Or `${day} ${month} ${year}` for space separation
 }
-var myDate = new Date(); // Get current date
-var formattedDate = formatToDDMMYYYY(myDate);
-//console.log(formattedDate);
-// const getAge = (birthday: string): number => {
-//     if (age) {
-//         return `Hello ${name} ${age}`;
-//     }else {
-//         return `Hello ${name}`;
-//     }
-// }
-// sayHi("John", 23);
+function getAge(dob) {
+    const getFormat = /^\d{4}\-(0[1-9]|1[0-2])\-(0[1-9]|[12][0-9]|3[01])$/;
+    const today = new Date();
+    const match = getFormat.exec(dob);
+    if (match) {
+        const givenYear = parseInt(dob.substring(0, 4));
+        const givenMonth = parseInt(dob.substring(5, 7));
+        const givenDay = parseInt(dob.substring(8, 10));
+        const currentMonth = today.getMonth() + 1;
+        const currentDay = today.getDate();
+        let age = today.getFullYear() - givenYear;
+        if (age < 0) {
+            let errorFound = 'Given year can not be greater than current year';
+            return errorFound;
+        }
+        // If the current month is earlier than the month of birth, or if it's the same month but the current day is earlier, subtract one year.
+        if (currentMonth < givenMonth || (currentMonth === givenMonth && currentDay < givenDay)) {
+            age--;
+        }
+        return age.toString();
+    }
+    else {
+        return 'Incorrect format, please validate given date: YYYY-MM-DD';
+    }
+}
+const myDate = new Date(); // Get current date
+const formattedDate = formatToDDMMYYYY(myDate);
+console.log(myDate);
+const result = getAge('2020-11-12');
+console.log(`The person is ${result} years old`);
--- js/app.ts
+++ js/app.ts
@@ -1,3 +1,4 @@
+
 function formatToDDMMYYYY(date: Date): string {
   const day = String(date.getDate()).padStart(2, '0');
   const month = String(date.getMonth() + 1).padStart(2, '0'); // Month is 0-indexed
@@ -5,16 +6,38 @@ function formatToDDMMYYYY(date: Date): string {
   return `${day}/${month}/${year}`; // Or `${day} ${month} ${year}` for space separation
 }
 
+function getAge(dob: string): string {
+  const getFormat = /^\d{4}\-(0[1-9]|1[0-2])\-(0[1-9]|[12][0-9]|3[01])$/;
+  const today = new Date();
+    const match = getFormat.exec(dob);
+    if (match) {        
+        const givenYear = parseInt(dob.substring(0, 4));
+        const givenMonth = parseInt(dob.substring(5, 7));
+        const givenDay = parseInt(dob.substring(8, 10));
+        const currentMonth = today.getMonth() + 1;
+        const currentDay = today.getDate();
+
+        let age = today.getFullYear() - givenYear;
+
+        if (age < 0) {
+            let errorFound = 'Given year can not be greater than current year';            
+            return errorFound;
+        }
+
+         // If the current month is earlier than the month of birth, or if it's the same month but the current day is earlier, subtract one year.
+        if (currentMonth < givenMonth || (currentMonth === givenMonth && currentDay < givenDay)) {
+            age--;
+        }
+        return age.toString();
+    }
+    else {
+      return 'Incorrect format, please validate given date: YYYY-MM-DD'
+    }
+}
+
 const myDate = new Date(); // Get current date
 const formattedDate = formatToDDMMYYYY(myDate);
-//console.log(formattedDate);
-// const getAge = (birthday: string): number => {
-//     if (age) {
-//         return `Hello ${name} ${age}`;
-//     }else {
+console.log(myDate);
 
-//         return `Hello ${name}`;
-//     }
-    
-// }
-// sayHi("John", 23);
+const result = getAge('2020-11-12');
+console.log(`The person is ${result} years old`);
